6f6984fcde7c453c6f82238916e405dad9d808d6
Lucas Vilas Boas Lage <lucasvilasboaslage@gmail.com>
2023-12-04 19:40:36 -0300

    Criando abstração de entidades

---
+++ src/Services/Catalogo/NSE.Catalogo.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Catalog.API.Models
+{
+    public class Product : Entity
+    {   
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
